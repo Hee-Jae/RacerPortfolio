cc9bf2b5716b91340eebc1ca16963b53a05fae1e
heejae <jhj967878@naver.com>
2021-08-17 03:34:14 +0900

    라우터 설정

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
+import {NavComponent, Main, Network, Login} from './portfolio/components';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import {Provider, useSelector} from "react-redux";
+import store from "./redux/store";
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavComponent />
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="/network">
+          <Network />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
