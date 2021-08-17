228ecc96d8b749acc86c06755a7fe7d8d3e07aac
heejae <jhj967878@naver.com>
2021-08-17 15:23:23 +0900

    포트폴리오 구성 요소 임시 생성

--- Client/src/portfolio/Main.js
+++ Client/src/portfolio/Main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import {Award, Certificate, Edu, Profile, Project} from './contents/all-contents'
 const Main = () => {
   const isLogin = useSelector((state) => state.auth);
   console.log(isLogin);
   return(
-    <h1> Main </h1>
+    <div>
+      <h1> Main </h1>
+      <Profile />
+      <Edu />
+      <Award />
+      <Project />
+      <Profile />
+    </div>
   );
 }
 
