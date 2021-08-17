2ccf7a4a85cbd723fc88d0bd78acabfad7c12f54
heejae <jhj967878@naver.com>
2021-08-17 22:27:35 +0900

    Migration 적용

--- Server/RacerPortfolio/app.py
+++ Server/RacerPortfolio/app.py
@@ -1,16 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+from db_connect import db
+from api.user_api import userbp
 from secret import secret_key
 import config
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(userbp)
     app.config.from_object(config)
-    db = SQLAlchemy(app)
+    db.init_app(app)
 
-    app.secret_key = secret_key
+    migrate = Migrate()
+    migrate.init_app(app, db, compare_type=True)
+    from models import user, award, edu, project, certificate
     
+    app.secret_key = secret_key
+    CORS(app)
+        
     return app
 
 if __name__ == "__main__":
-    create_app().run()
+    create_app().run(debug=True) # 배포할 때 debug=True 안지우면 오랑우탄
