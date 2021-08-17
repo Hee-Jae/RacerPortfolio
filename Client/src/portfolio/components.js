154adb47e3fad1fd4de4ba775a46456dd2b1c9a5
heejae <jhj967878@naver.com>
2021-08-17 15:23:01 +0900

    register 컴포넌트 추가

--- Client/src/portfolio/components.js
+++ Client/src/portfolio/components.js
@@ -4,3 +4,4 @@ export {default as Network} from "./Network";
 export {default as Posts} from "./Posts";
 export {default as SearchBar} from "./SearchBar";
 export {default as Login} from "./login/Login";
+export {default as Register} from "./register/Register";
