91e1288764bd3105724fb9f106b94cd8950aebb3
heejae <jhj967878@naver.com>
2021-08-17 15:22:35 +0900

    로그인 성공 시 메인페이지로

--- Client/src/portfolio/login/LoginForm.js
+++ Client/src/portfolio/login/LoginForm.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import { useHistory } from "react-router-dom";
 import {login} from "../../redux/action";
 const LoginForm = () => {
 
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
   const dispatch = useDispatch();
-  const clickHandler = () => {
-    dispatch(login({id: id, pw: pw}))
+  const isLogin = useSelector((state) => state.auth);
+  const history = useHistory();
+
+  const loginHandler = () => {
+    dispatch(login({id: id, pw: pw}));
+    // isLogin ? history.push('/') : alert("로그인 실패");
+    history.push('/');
   };
 
   return(
@@ -16,7 +22,7 @@ const LoginForm = () => {
       <p>아이디</p><input type="text" value={id} onChange={e => setId(e.target.value)} />
       <p>비밀번호</p><input type="password" value={pw} onChange={e => setPw(e.target.value)} />
     </form>
-    <button type="submit" onClick={clickHandler}> 로그인 </button>
+    <button type="submit" onClick={loginHandler}> 로그인 </button>
     </>
   );
 }
