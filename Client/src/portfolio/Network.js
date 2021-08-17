f73c87ed1c6bcf4cf337ea3f04c1eba7eeba6792
heejae <jhj967878@naver.com>
2021-08-17 15:22:48 +0900

    네트워크 메뉴 임시 생성

--- Client/src/portfolio/Network.js
+++ Client/src/portfolio/Network.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import Posts from './Posts';
 
 const Network = () => {
   return(
-    <h1> Network </h1>
+    <div>
+      <h1> Network </h1>
+      <SearchBar />
+      <Posts />
+    </div>
   );
 }
 
--- Client/src/portfolio/Posts.js
+++ Client/src/portfolio/Posts.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Posts = () => {
   return(
-    <h1> Posts </h1>
+    <div>
+      <img></img>
+      <p> 이름 </p>
+      <p> 한줄 소개 </p>
+      <button> 정보보기 </button>
+    </div>
   );
 }
 
