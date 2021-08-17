7d559dc89271682132a1ad64e8779a8147b54b35
heejae <jhj967878@naver.com>
2021-08-17 15:23:36 +0900

    포트폴리오 구성 요소 임시 생성

---
+++ Client/src/portfolio/contents/all-contents.js
@@ -0,0 +1,5 @@
+export {default as Award} from "./Award";
+export {default as Certificate} from "./Certificate";
+export {default as Edu} from "./Edu";
+export {default as Profile} from "./Profile";
+export {default as Project} from "./Project";
