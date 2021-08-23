cb4f36d1ab48e953ddda4444b0ae006f2dba44e8
heejae <jhj967878@naver.com>
2021-08-23 10:57:19 +0900

    isLoggedIn -> isLogined 변수명 변경

--- Client/src/portfolio/Nav.js
+++ Client/src/portfolio/Nav.js
@@ -11,7 +11,7 @@ import { logout } from 'redux/action';
 const NavComponent = (props) => {
 
   const dispatch = useDispatch();
-  const isLogin = useSelector((state) => state.user.isLoggedIn);
+  const isLogin = useSelector((state) => state.user.isLogined);
   const user_id = useSelector((state) => state.user.user_id);
 
   const logoutHandler = () => {
@@ -25,10 +25,10 @@ const NavComponent = (props) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <Link to={`/main?user=${user_id}`} className="nav-link">메인</Link>
+          <Link to="/main" className="nav-link">메인</Link>
           <Link to="/network" className="nav-link">네트워크</Link>
           {isLogin ?
-          (<Link to="" className="nav-link" onClick={logoutHandler}>로그아웃</Link>) :
+          (<Link to="/login" className="nav-link" onClick={logoutHandler}>로그아웃</Link>) :
           (<Link to="/login" className="nav-link">로그인</Link>)}
           </Nav>
         </Navbar.Collapse>
